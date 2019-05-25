860c80aef6430936adf6af51c7bd9537b734111a
kromchenko <kromchenko1987@gmail.com>
2019-05-25 11:56:12 +0300

    create simple counter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+      <div>
+        <button class="btn" @click="clickCounterHunter">
+          Click counter {{$store.state.count}}
+        </button>
+      </div>
   </div>
 </template>
 
@@ -9,8 +14,22 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods:{
+    clickCounterHunter(){
+      console.log("clickCounterHunter");
+      this.$store.commit('setCount', 3)
+    }
+  },
+  mounted(){
+    console.log(this.$store.state.count);
+  },
+  computed:{
+    count(){
+      return this.$store.state.count;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -29,4 +48,14 @@ li {
 a {
   color: #42b983;
 }
+
+  .btn{
+    border: 1px solid #42b983;
+    background-color: #fff;
+    color: #42b983;
+    padding: 10px 20px;
+    border-radius: 4px;
+    outline: unset;
+    cursor: pointer;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count:0
+  },
+  mutations:{
+    setCount(state){
+      console.log("setCount");
+      state.count++;
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
